e1b80beaf1b0aabcadcda4a4e990c5b2cf53aa3d
destino92 <destinodello@gmail.com>
2016-12-08 01:28:32 +0300

    feat: added routes for app component and storePicker component

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+
 /*
   App
 */
@@ -74,4 +79,14 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+/*
+  Routes
+*/
+var routes = (
+  <Router>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
