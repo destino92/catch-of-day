5fa117297c0839259d012c4c2ad2e306302c0e51
destino92 <destinodello@gmail.com>
2016-12-11 02:30:53 +0300

    chore: converted react component to es6 class

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -8,6 +8,8 @@ import Fish from './Fish';
 import Order from './Order';
 import Inventory from './Inventory';
 import Catalyst from 'react-catalyst';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
 // Firebase
 import Rebase from 're-base';
@@ -19,15 +21,19 @@ var base = Rebase.createClass({
     messagingSenderId: "435226083365"
   });
 
-var App = React.createClass({
-  mixins: [Catalyst.LinkedStateMixin],
-  getInitialState: function(){
-    return {
+@autobind
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
       fishes: {},
       order: {}
     }
-  },
-  componentDidMount: function(){
+  }
+
+  componentDidMount(){
     base.syncState(this.props.params.storeId + '/fishes', {
       context : this,
       state : 'fishes'
@@ -41,44 +47,48 @@ var App = React.createClass({
         order: JSON.parse(localStorageRef)
       });
     }
-  },
-  componentWillUpdate: function(nextProps, nextState){
+  }
+
+  componentWillUpdate(nextProps, nextState){
     localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
-  },
-  addToOrder: function(key){
+  }
+
+  addToOrder(key){
     this.state.order[key] = this.state.order[key] + 1 || 1;
     this.setState({ order: this.state.order })
-  },
-  removeFromOrder: function(key){
+  }
+
+  removeFromOrder(key){
     delete this.state.order[key];
     this.setState({
       order: this.state.order
     });
-  },
-  addFish: function(fish){
+  }
+
+  addFish(fish){
     var timestamp = (new Date()).getTime();
     // Update the state object
     this.state.fishes['fish-' + timestamp] = fish;
     // set the state
     this.setState({ fishes : this.state.fishes});
-  },
-  removeFish: function(key){
+  }
+  removeFish(key){
     if(confirm("Are you sure you want to remove " + this.state.fishes[key].name + " from the store?")){
       this.state.fishes[key] = null;
       this.setState({
         fishes: this.state.fishes
       });
     }
-  },
-  loadSamples: function(){
+  }
+  loadSamples(){
     this.setState({
       fishes: require('../sample-fishes')
     })
-  },
-  renderFish: function(key){
+  }
+  renderFish(key){
     return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
-  },
-  render: function() {
+  }
+  render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
@@ -88,10 +98,13 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder}/>
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish}/>
+        {/* bind this.linkState because the mixin is define after the function */}
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState.bind(this)} removeFish={this.removeFish}/>
       </div>
     )
   }
-});
+};
+
+reactMixin.onClass(App, Catalyst.LinkedStateMixin);
 
 export default App;
--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -4,18 +4,22 @@
 import React from 'react';
 import { History } from 'react-router'; // mixin
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins: [History],
-  goToStore: function(event){
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event){
     event.preventDefault();
     // get the data from the input
 
     var storeId = this.refs.storeId.value;
     // transition from <StorePicker/> to <App/> using // History mixin
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render: function() {
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -24,6 +28,9 @@ var StorePicker = React.createClass({
       </form>
     )
   }
-});
+
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
